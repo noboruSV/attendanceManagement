e53e83f0e2d12b904f67af13611933fa01ff119b
noboru.m <noboru@softventure.co.jp>
2020-10-05 16:10:11 +0900

    MenuController.javaを/web/menu配下に移動

--- main/java/jp/co/softventure/web/menu/MenuController.java
+++ main/java/jp/co/softventure/web/menu/MenuController.java
@@ -1,43 +1,60 @@
 package jp.co.softventure.web.menu;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
-import jp.co.softventure.web.login.LoginForm;
+import jp.co.softventure.bean.SessionBean;
+import jp.co.softventure.domain.LoginData;
+import jp.co.softventure.service.DBLoginDataService;
 
 @Controller
+@SessionAttributes("scopedTarget.sessionBean")
 public class MenuController {
-	@ModelAttribute(value="menuForm")
-	public MenuForm menuForm() {
-		return new MenuForm();
+	
+	@Autowired
+	private DBLoginDataService service;
+	
+	@Autowired
+	public SessionBean sessionBean;
+	
+	@ModelAttribute(value = "dispUserName")
+	public LoginData dispUserName() {
+		return new LoginData();
 	}
-	@ModelAttribute(value="loginForm")
-	public LoginForm loginForm(LoginForm loginForm) {
-		return new LoginForm();
+	
+	@RequestMapping(value = "/menu", params = "registration")
+	public String toRegistrationPage() {
+		return "redirect:/registration";
 	}
-
-	@RequestMapping(value="/menu")
-	public String menu1(@ModelAttribute("loginForm") LoginForm loginForm, Model model) {
-			return "menu/menu";
+	
+	@RequestMapping(value = "/menu", params = "workReportsList")
+	public String toWorkReportList() {
+		return "redirect:/workReportsList";
 	}
-	@RequestMapping(value="/menu", params = "jikoku")
-	public String menu() {
-		return "jikoku/jikokuInputt";
+	
+	@RequestMapping(value = "/menu", params = "logout")
+	public ModelAndView toLogout(@ModelAttribute("dispUserName") LoginData loginData, SessionStatus sessionStatus, ModelAndView mav) {
+		loginData.setId(sessionBean.getId());
+		List<LoginData> list = service.selectUserName(loginData);
+		if ( list.size() == 1 ) {
+			for ( int i = 0; i < list.size(); i++ ) {
+				loginData = list.get(i);
+				mav.addObject("dispUserName", loginData);
+			}
+		} else {
+			;
+		}
+		//セッション破棄処理
+		sessionStatus.setComplete();
+		mav.setViewName("logout");
+		return mav;
 	}
-
-	@RequestMapping(value="/menu", params = "workReportList" )
-	public String toWorkReportList(@ModelAttribute("loginForm") LoginForm loginform, MenuForm menuform, BindingResult result, Model model) {
-		//一覧画面へ遷移
-//		model.addAttribute("model", model);
-		return "forward:workReportsList";
-	}
-
-	@RequestMapping(value="/menu", params = "logout" )
-	public String toLogout(@ModelAttribute("loginForm") LoginForm loginform, MenuForm menuform, BindingResult result, Model model) {
-			return "logout/logout";
-	}
-
+	
 }
