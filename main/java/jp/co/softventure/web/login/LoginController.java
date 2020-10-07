4f21e25b6d07bc4655cef7129b9d519a224a07f1
noboru.m <noboru@softventure.co.jp>
2020-10-07 14:52:16 +0900

    2020/10/05　16：06：27の変更を取り消し

--- main/java/jp/co/softventure/web/login/LoginController.java
+++ main/java/jp/co/softventure/web/login/LoginController.java
@@ -1,8 +1,8 @@
-/**
- * 2020/10/03 新規作成 Noboru.M
- */
 package jp.co.softventure.web.login;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,75 +10,99 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.thymeleaf.util.StringUtils;
 
-import jp.co.softventure.domain.dto.LoginDto;
-import jp.co.softventure.model.LoginDataInfo;
-import jp.co.softventure.service.login.LoginService;
-
+import jp.co.softventure.domain.LoginData;
+import jp.co.softventure.domain.SearchLoginData;
+import jp.co.softventure.service.DBLoginDataService;
+import jp.co.softventure.web.db.SearchLoginDataForm;
+import jp.co.softventure.web.menu.MenuForm;
+import jp.co.softventure.bean.SessionBean;
 
 @Controller
-@SessionAttributes("loginDataInfo")
 public class LoginController {
 	
 	@Autowired
-	private LoginService loginService;
+	private DBLoginDataService service;
+
+	@Autowired
+	protected SessionBean sessionBean;
+	
+	@ModelAttribute(value = "searchLoginDataForm")
+	public SearchLoginDataForm setForm() {
+		return new SearchLoginDataForm();
+	}
 	
 	@ModelAttribute(value = "loginForm")
 	public LoginForm setLoginForm() {
 		return new LoginForm();
 	}
 
-	@ModelAttribute(value = "loginDataInfo")
-	public LoginDataInfo loginDataInfo() {
-		return new LoginDataInfo();
-	}
-	
 	/**
 	 * ログイン画面表示
 	 * @param loginForm
 	 * @return
 	 */
 	@RequestMapping("/login")
-	public String login() {
+	public String login(LoginForm loginForm) {
 
 		return "login/login";
 
 	}
 
-	/**
-	 * ログイン成功⇒メニュー画面へ遷移　
-	 * ログイン失敗⇒ログイン画面へ遷移
-	 * @param searchLoginDataForm
-	 * @param loginForm
-	 * @param result
-	 * @param model
-	 * @return
-	 */
+	
 	@RequestMapping(value="/login", params="jdg")
 	public String loginFailed(
-			@ModelAttribute LoginDataInfo loginDataInfo,
+			@ModelAttribute("searchLoginDataForm") SearchLoginDataForm searchLoginDataForm,
 			@Validated LoginForm loginForm, BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
 			return "login/login";
 		}
 		
-		//ログイン認証
-		loginForm = loginService.loginCheck(loginForm, model);
+		loginForm.setLoginFlg((short)0);
+		sessionBean.setId(loginForm.getId());
 		
-		//loginDataInfoにログイン情報を詰めて持ち回る
-		loginDataInfo.setLoginJdgFlg(loginForm.getLoginJdgFlg());
-		loginDataInfo.setUserId(loginForm.getUserId());
-		loginDataInfo.setUserName(loginForm.getUserName());
-		if (StringUtils.equals(loginDataInfo.getLoginJdgFlg(), "1")){
-			model.addAttribute("loginDataInfo", loginDataInfo);
-			return "forward:menu";
+		//loginを取得する
+		// データ検索に利用するドメインクラスのインスタンス化
+		SearchLoginData searchLoginData = new SearchLoginData();
+		// Formクラスの値をドメインクラスにコピー
+		BeanUtils.copyProperties(loginForm, searchLoginData);
+		// 検索を行うためのサービス処理呼び出し
+		List<LoginData> list = service.searchLoginData(searchLoginData);
+		
+		if (list!=null && list.size()!=0) {
+			// Modelオブジェクトに検索結果を格納
+			// model.addAttribute("loginDataList", list);
+			String id = list.get(0).getId();
+			String pw = list.get(0).getPw();
+
+			loginForm.setId(id);
+			loginForm.setUserName(list.get(0).getUserName());
+
+			MenuForm menuForm = new MenuForm();
+			menuForm.setId(id);
+			menuForm.setUserName(list.get(0).getUserName());
+			model.addAttribute("menuForm", menuForm);
+			
+			// 画面．パスワード ＝ login_data．パスワードの場合、
+			// 作業時間・内容　登録画面へ遷移する
+			if(StringUtils.equals(loginForm.getId(), id)
+					&& StringUtils.equals(loginForm.getPw(), pw)) {
+				loginForm.setPw("");
+				model.addAttribute("loginForm", loginForm);
+//				return "forward:menu";
+				return "menu/menu";
+
+			} else if (!StringUtils.equals(loginForm.getPw(), pw)) {
+				// ログインフラグ＝2：パスワードが違います。
+				loginForm.setLoginFlg((short)2);
+				return "login/login";
+			}
 		}
-
+		// ログインフラグ＝1：ログインIDが存在しません。
+		loginForm.setLoginFlg((short)1);
 		return "login/login";
-
 	}
 
 }
