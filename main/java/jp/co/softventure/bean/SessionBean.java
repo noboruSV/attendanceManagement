ebc59301ee76218ee3754ce6019b1bddf03fe476
MotekiSV <syunpei.mtk@softventure.co.jp>
2020-12-08 09:15:40 +0900

    フィールド追加

--- main/java/jp/co/softventure/bean/SessionBean.java
+++ main/java/jp/co/softventure/bean/SessionBean.java
@@ -1,19 +1,32 @@
 package jp.co.softventure.bean;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+
 import org.springframework.stereotype.Component;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.web.context.WebApplicationContext;
+//import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.ScopedProxyMode;
+//import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.annotation.SessionScope;
 
+//@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode=ScopedProxyMode.TARGET_CLASS)
 @Component
-@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode=ScopedProxyMode.TARGET_CLASS)
-
+@SessionScope
 public class SessionBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	private String sessionId;
 	private String id;
 	private String userName;
+	private Timestamp recUpdtDt;
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
 
 	public String getId() {
         return id;
@@ -31,4 +44,12 @@ public class SessionBean implements Serializable {
 		this.userName = userName;
 	}
 
+	public Timestamp getRecUpdtDt() {
+		return recUpdtDt;
+	}
+
+	public void setRecUpdtDt(Timestamp recUpdtDt) {
+		this.recUpdtDt = recUpdtDt;
+	}
+	
 }
