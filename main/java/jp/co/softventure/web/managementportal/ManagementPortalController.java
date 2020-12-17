7798c8d492de47665dc27eeeefbd3dc4b201797c
MotekiSV <syunpei.mtk@softventure.co.jp>
2020-12-17 14:15:43 +0900

    削除猶予期間を定数に変更

--- main/java/jp/co/softventure/web/managementportal/ManagementPortalController.java
+++ main/java/jp/co/softventure/web/managementportal/ManagementPortalController.java
@@ -398,23 +398,25 @@ public class ManagementPortalController {
 	public String calcDeletableDate(Timestamp recUpdtDt) {
 		Date date1 = new Date();
 		Date date2 = new Date(recUpdtDt.getTime());
-		String str = diffDays(date1, date2);
-		return str;
+		String deletableDate = diffDates(date1, date2);
+		return deletableDate;
 	}
 	
 	//ふたつの日付を差分するメソッド
-	public String diffDays(Date date1,Date date2) {
-	    long datetime1 = date1.getTime();
+	public String diffDates(Date date1,Date date2) {
+	    //削除猶予期間
+		final int gracePeriod = 30;
+		long datetime1 = date1.getTime();
 	    long datetime2 = date2.getTime();
 	    long oneDateTime = 1000 * 60 * 60 * 24;
-	    long diffDays = (datetime1 - datetime2) / oneDateTime;
-	    String str;
-	    if ( diffDays >= 30 ) {
-	    	 str = "(削除可能)";
+	    long diff = (datetime1 - datetime2) / oneDateTime;
+	    String deletableDate;
+	    if ( diff >= gracePeriod ) {
+	    	deletableDate = "(削除可能)";
 	    } else {
-	    	 str = "(あと" + String.valueOf(30 - diffDays) + "日で削除可能)";
+	    	deletableDate = "(あと" + String.valueOf(gracePeriod - diff) + "日で削除可能)";
 	    }
-	    return str;
+	    return deletableDate;
 	}
 	
 	//アクセス認証チェック
